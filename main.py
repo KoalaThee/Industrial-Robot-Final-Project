c35b38e77a18c400c5a80badd7c0e60a1845aaa5
Illu10boy <60164026+Illu10boy@users.noreply.github.com>
2025-04-24 23:20:04 +0700

    add comment

--- main.py
+++ main.py
@@ -15,8 +15,6 @@ c_port = 2002
 a_ip  = "10.10.0.14"
 a_port  = 30003
 
-conveyor_speed = 20
-
 s_port = 2024
 
 offset = 0.2
@@ -35,22 +33,24 @@ def main():
     arm.home()
     # go to home position
 
+    #receive predefined rotation angle 
     _, _, _, rxi, ryi, rzi, _ = arm.baannai()
     gripper.open()
     time.sleep(1.5)
 
     @s.listen
     def handle_message(msg: str):
+        #split message from vision builder into python list
         msg = msg[1:-2].split(",")
-        # print(msg)
         processed_msg = [e if e != "" else "None" for e in msg]
-        # print(processed_msg)
         if processed_msg[0] == 'True':
 
+                #check length of the message to avoid false data from buffer
                 if len(processed_msg) == 5:
                     x = int(processed_msg[1])/1000 + 0.29279 - offset
                     y = int(processed_msg[2])/1000 - 0.32984 + 0.015
                     time.sleep(2)
+                    #flip the robot 180 degree
                     arm.movej(
                          x=arm.HOME_X,
                          y=arm.HOME_Y,
@@ -61,6 +61,7 @@ def main():
                          relative=False
                     )
                     time.sleep(3)
+                    #move the UR arm to pick up point
                     arm.movel(
                         x = x,
                         y = y,
@@ -74,6 +75,7 @@ def main():
 
                     gripper.close()
                     time.sleep(1)
+                    #pick up the box
                     arm.movej(
                          x=arm.HOME_X,
                          y=arm.HOME_Y,
@@ -85,6 +87,7 @@ def main():
                          t=1
                     )
                     time.sleep(1)
+                    #return to initial position
                     arm.home() 
                     gripper.open()
                     time.sleep(0.5)
@@ -94,8 +97,7 @@ def main():
             
 if __name__ == "__main__":
     main()
-    # if input() == "q":
-    #     conveyor.stop_conveyor()
+
 
 
 
